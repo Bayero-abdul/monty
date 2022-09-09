93e1884a0cdc57696118ee6a1882a376b2a26852
Bayero-abdul <bayeroa65@gmail.com>
2022-09-09 19:25:49 +0000

    Correct pint() output

--- stack.c
+++ stack.c
@@ -70,7 +70,7 @@ void pint(stack_s **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
