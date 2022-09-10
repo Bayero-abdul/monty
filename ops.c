3d5728fa9134f7bf93c1fab66d4eaa703e1d4dc8
Bayero-abdul <bayeroa65@gmail.com>
2022-09-10 09:26:49 +0000

    Correct phar_t() error output

--- ops.c
+++ ops.c
@@ -32,7 +32,7 @@ void pchar_t(stack_s **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pchar, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
