8824401e58290610b8db4a93b670923f8fa9c2e9
Bayero-abdul <bayeroa65@gmail.com>
2022-09-10 09:24:59 +0000

    Push the ops.c file

--- ops.c
+++ ops.c
@@ -22,3 +22,25 @@ void swap(stack_s **stack, unsigned int line_number)
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = temp;
 }
+
+/**
+* pchar_t - prints the char at the top of the stack
+* @stack: stack
+* @line_number: line number current been executed
+*/
+void pchar_t(stack_s **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", (*stack)->n);
+}
